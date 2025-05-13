b6528974b03974a32fdf3685e51f41307e8b7a4c
mohanrav <mohanrav@gmail.com>
2025-05-13 16:18:26 -0400

    fixed the cred

--- Program.cs
+++ Program.cs
@@ -6,12 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container
 builder.Services.AddControllersWithViews();
 
-AWS_ACCESS_KEY_ID="AKIAZBQE345LKPTEAHQD"
-AWS_SECRET_ACCESS_KEY="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"
 
 // Add database context (this is added above route mapping)
 builder.Services.AddDbContext<SalesDbContext>(options =>
-    options.UseSqlServer("Server=localhost\\SpringHoneyLocal;Database=SalesDatabase;User Id = sa;Password="$pringH0n3y$$25";TrustServerCertificate=True;"));
+    options.UseSqlServer("Server=localhost\\SpringHoneyLocal;Database=SalesDatabase;User Id = sa;Password=$pringH0n3y$$25;TrustServerCertificate=True;"));
 
 var app = builder.Build();
 
