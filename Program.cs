4deed87d11080c40de9987a9bc72b3bafc584c18
mohanrav <mohanrav@gmail.com>
2025-09-15 15:12:41 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,8 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container
 builder.Services.AddControllersWithViews();
 
-AWS_ACCESS_KEY_ID="wewewewewe "
-AWS_SECRET_ACCESS_KEY="weweweweweweweew"
 
 
 
