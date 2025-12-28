ef33bc26a5c3c5f397339c650b3552831c7d4b6f
mohanrav <mohanrav@gmail.com>
2025-12-28 13:15:04 -0500

    Test

--- obj/Debug/net8.0/DailySalesTracker.AssemblyInfo.cs
+++ obj/Debug/net8.0/DailySalesTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DailySalesTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bf4b61b4bada59a23b1ff4362936a2672c62b2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fff73bcf582d549e1f927b31f74358305191bc14")]
 [assembly: System.Reflection.AssemblyProductAttribute("DailySalesTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DailySalesTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
